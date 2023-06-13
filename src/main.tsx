55c3110ac65df4afe988c876837e7cc80357d638
Daniel <dharbrueger@gmail.com>
2023-06-12 22:52:03 -0400

    Removed react-router-dom and related packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./index.css";
 import { SetBits } from "./setBits/SetBits";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SetBits />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <SetBits />
       </Provider>
   </React.StrictMode>
 );
